e51c32615fc7391e85c81e36ffd1cbe78d98125c
Tolik <anatoliymandrichenko@gmail.com>
2016-04-26 10:21:53 +0300

    merge to master

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(GateContract $gate)
     {
+
         $this->registerPolicies($gate);
 
        foreach ($this->getPermissions() as $permission){
